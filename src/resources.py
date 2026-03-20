24a0a37ebe84c17e49e2cf3c3906757114c1f2b0
heval0711 <25yz.silber.vincent@gmail.com>
2026-03-20 01:22:41 +0100

    Add quiz categories and questions to resources.py

---
+++ src/resources.py
@@ -0,0 +1,398 @@
+QUIZ_CATEGORIES = {
+    "Islam": [
+        {
+            "question": "How many Sajdāt of Tilāwah are there according to Shāfiʿiyyah?",
+            "options": {
+            "A": "14",
+            "B": "15",
+            "C": "13",
+            "D": "11",
+            },
+            "answer": "A",
+            "author": "al-Kūfī#9260"
+        },
+        {
+            "question": "The conditions for Ghusl sufficing both as Ghusl and as Wuḍūʾ are:",
+            "options": {
+            "A": "That the Ghusl be obligatory",
+            "B": "That the Ghusl be obligatory and you do not perform something that invalidates Wuḍūʾ while doing Ghusl",
+            "C": "That you do not perform something that invalidates Wuḍūʾ while doing Ghusl",
+            "D": "None of the above",
+            },
+            "answer": "B",
+            "author": "al-Kūfī#9260"
+        },
+        {
+            "question": "The “Shaykhān” refers to",
+            "options": {
+            "A": "al-Rāfiʿī and al-Ghazālī",
+            "B": "al-Nawawī and al-Ramlī",
+            "C": "al-Rāfiʿī and al-Nawawī",
+            "D": "al-Shīrāzī and al-Baghawī",
+            },
+            "answer": "C",
+            "author": "al-Kūfī#9260"
+        },
+        {
+            "question": "The Imām who highly criticized al-Isnawī and insisted that the Shaykhān should be given ultimate authority in the Madhhab was",
+            "options": {
+            "A": "Ibn Ḥajar al-Haytamī",
+            "B": "Shaykh al-Islām",
+            "C": "al-Māwardī",
+            "D": "Ibn Qāsim",
+            },
+            "answer": "B",
+            "author": "al-Kūfī#9260"
+        },
+        {
+            "question": "Whenever “Shaykh al-Islām” is mentioned without specification in our Fiqh books, it refers to",
+            "options": {
+            "A": "Ibn Ḥajar al-ʿAsqalānī",
+            "B": "al-Nawawī",
+            "C": "Ibn Taymiyyah",
+            "D": "Zakariyyā al-Anṣārī",
+            },
+            "answer": "D",
+            "author": "al-Kūfī#9260"
+        },
+        {
+            "question": "The two famous paths of the transmission of the Shāfiʿī Madhhab are:",
+            "options": {
+                "A": "ʿIrāqiyyīn and Baṣriyyīn",
+                "B": "Baṣriyyīn and Kūfiyyīn",
+                "C": "ʿIrāqiyyīn and Kūfiyyīn",
+                "D": "Khurasāniyyīn and ʿIrāqiyyīn",
+            },
+            "answer": "D",
+            "author": "al-Kūfī#9260"
+        },
+        {
+            "question": "The integrals of Wuḍūʾ in the Shāfiʿī Madhhab are:",
+            "options": {
+                "A": "7",
+                "B": "4",
+                "C": "6",
+                "D": "10",
+            },
+            "answer": "C",
+            "author": "al-Kūfī#9260"
+        },
+        {
+            "question": "Which great Shāfiʿī scholar held that the feet of the woman are not ʿAwrah?",
+            "options": {
+                "A": "al-Ghazālī",
+                "B": "al-Muzanī",
+                "C": "al-Rabīʿ",
+                "D": "al-Juwaynī",
+            },
+            "answer": "B",
+            "author": "al-Kūfī#9260"
+        },
+        {
+            "question": "The Imām of the ʿIrāqī path was:",
+            "options": {
+                "A": "Abū Ḥāmid al-Isfarāyīnī",
+                "B": "Abū Isḥāq al-Isfarāyīnī",
+                "C": "Abū Ḥāmid al-Ghazālī",
+                "D": "Abū Isḥāq al-Shīrāzī",
+            },
+            "answer": "A",
+            "author": "al-Kūfī#9260"
+        },
+        {
+            "question": "The Arkān of Ṣalāh are:",
+            "options": {
+                "A": "13",
+                "B": "15",
+                "C": "20",
+                "D": "11",
+            },
+            "answer": "A",
+            "author": "al-Kūfī#9260"
+        },
+        {
+            "question": "The baseline for Shāfiʿī Fatwā upon the Muʿtamad is:",
+            "options": {
+                "A": "al-Rawḍah",
+                "B": "al-Muḥarrar",
+                "C": "al-Minhāj",
+                "D": "al-Wajīz",
+            },
+            "answer": "C",
+            "author": "al-Kūfī#9260"
+        },
+        {
+            "question": "The author of the great Fiqh book “Baḥr al-Madhhab” (Sea of the Madhhab) was:",
+            "options": {
+                "A": "al-Rūyānī",
+                "B": "Ibn Khuzaymah",
+                "C": "al-Shirbīnī",
+                "D": "al-Mutawallī",
+            },
+            "answer": "A",
+            "author": "al-Kūfī#9260"
+        },
+    ],
+    "Sports": [],
+    "General-Knowledge": [
+        {
+            "question": "What is the name of this symbol: ¶?",
+            "options": {
+                "A": "Fermata",
+                "B": "Pilcrow",
+                "C": "Interrobang",
+                "D": "Octothorpe",
+            },
+            "answer": "B",
+        },
+        {
+            "question": "What is a Pomelo?",
+            "options": {
+                "A": "A Punching Bag",
+                "B": "An Old Football",
+                "C": "A Breed of Dog",
+                "D": "A Citrus Fruit",
+            },
+            "answer": "D",
+        },
+        {
+            "question": "If you \"decimate\" something, what percentage of it do you destroy?",
+            "options": {
+                "A": "10%",
+                "B": "90%",
+                "C": "100%",
+                "D": "None of the above",
+            },
+            "answer": "A",
+        },        
+    ],
+    "Cetaceans": [
+        {
+            "question": "'Cetacean' is the name given to the order of animals such as whales, dolphins and porpoises. The ancient Greeks named these animals 'ketos'. What does/did it mean?",
+            "options": {
+                "A": "Dragon",
+                "B": "Serpent",
+                "C": "Sea Monster",
+                "D": "Fish",
+            },
+            "answer": "C",
+        },
+        {
+            "question": "Cetaceans are divided into two categories. What are they, in their Latin form?",
+            "options": {
+                "A": "Odontocetes and Megaptera",
+                "B": "Musculus and Phocena",
+                "C": "Balaenoptera and Mysticetes",
+                "D": "Odontocetes and Mysticetes",
+            },
+            "answer": "A",
+        },
+        {
+            "question": "Which of the following are members of the toothed whale family?",
+            "options": {
+                "A": "Sperm whale and spinner dolphin",
+                "B": "Bryde's whale and orca",
+                "C": "Sperm whale and humpback whale",
+                "D": "Bottlenose dolphin and minke whale",
+            },
+            "answer": "C",
+        },
+        {
+            "question": "What is the name of the notch in the tail flukes of whales dolphins and porpoises?",
+            "options": {
+                "A": "Peduncle notch",
+                "B": "Super-sternal notch",
+                "C": "Cetas notch",
+                "D": "Median notch",
+            },
+            "answer": "D",
+        },
+        {
+            "question": "Paul Spong is world famous for his work researching which member of the Odontocete family?",
+            "options": {
+                "A": "Bottlenose dolphins",
+                "B": "Northern bottlenose whales",
+                "C": "Pantropical spotted dolphins",
+                "D": "Orcas",
+            },
+            "answer": "D",
+        },
+        {
+            "question": "In which country are thousands of small toothed whales slaughtered every year, September through to March, for 'pest control' reasons, but actually for food? The Oscar winning documentary 'The Cove' covered this subject.",
+            "options": {
+                "A": "USA",
+                "B": "Iceland",
+                "C": "Japan",
+                "D": "Norway",
+            },
+            "answer": "C",
+        },
+        {
+            "question": "Bottlenose dolphins are found in waters all over the globe. There are two sites in the UK where they are found in their hundreds! Where?",
+            "options": {
+                "A": "Moray Firth (Scotland) and Cardigan Bay (Wales)",
+                "B": "Folkestone (England) and Moray Firth (Wales)",
+                "C": "Moray Firth (Scotland) & Perenporth (Cornwall)",
+                "D": "Biddeford (England) and Cardigan Bay (Wales)",
+            },
+            "answer": "A",
+        },
+        {
+            "question": "Which baleen whale was referred to as the 'angel of the sea' by ancient whalers?",
+            "options": {
+                "A": "Sperm whale",
+                "B": "Beluga whale",
+                "C": "Humpback whale",
+                "D": "Right whale",
+            },
+            "answer": "C",
+        },
+        {
+            "question": "Phocena phocena are often beaten up by which odontocete?",
+            "options": {
+                "A": "Bottlenose dolphins",
+                "B": "Hectors dolphins",
+                "C": "Beluga whales",
+                "D": "Blue whales",
+            },
+            "answer": "A",
+        },
+        {
+            "question": "A rorqual is another name for a baleen whale, and refers to the expandable pleats under the chin. (It comes from the Norwegian word røyrkval, meaning 'furrow whale'). Which rorqual has white bands across its pectoral fins?",
+            "options": {
+                "A": "Narwhal",
+                "B": "Ganges river dolphin",
+                "C": "Boto",
+                "D": "Minke whale",
+            },
+            "answer": "D",
+        },        
+    ]
+}
+
+QUIZ_QUESTIONS = [
+    {
+        "question": "How many Sajdāt of Tilāwah are there according to Shāfiʿiyyah?",
+        "options": {
+        "A": "14",
+        "B": "15",
+        "C": "13",
+        "D": "11",
+        },
+        "answer": "A",
+    },
+    {
+        "question": "The conditions for Ghusl sufficing both as Ghusl and as Wuḍūʾ are:",
+        "options": {
+        "A": "That the Ghusl be obligatory",
+        "B": "That the Ghusl be obligatory and you do not perform something that invalidates Wuḍūʾ while doing Ghusl",
+        "C": "That you do not perform something that invalidates Wuḍūʾ while doing Ghusl",
+        "D": "None of the above",
+        },
+        "answer": "B",
+    },
+    {
+        "question": "The “Shaykhān” refers to",
+        "options": {
+        "A": "al-Rāfiʿī and al-Ghazālī",
+        "B": "al-Nawawī and al-Ramlī",
+        "C": "al-Rāfiʿī and al-Nawawī",
+        "D": "al-Shīrāzī and al-Baghawī",
+        },
+        "answer": "C",
+    },
+    {
+        "question": "The Imām who highly criticized al-Isnawī and insisted that the Shaykhān should be given ultimate authority in the Madhhab was",
+        "options": {
+        "A": "Ibn Ḥajar al-Haytamī",
+        "B": "Shaykh al-Islām",
+        "C": "al-Māwardī",
+        "D": "Ibn Qāsim",
+        },
+        "answer": "B",
+    },
+    {
+        "question": "Whenever “Shaykh al-Islām” is mentioned without specification in our Fiqh books, it refers to",
+        "options": {
+        "A": "Ibn Ḥajar al-ʿAsqalānī",
+        "B": "al-Nawawī",
+        "C": "Ibn Taymiyyah",
+        "D": "Zakariyyā al-Anṣārī",
+        },
+        "answer": "D",
+    },
+    {
+        "question": "The two famous paths of the transmission of the Shāfiʿī Madhhab are:",
+        "options": {
+            "A": "ʿIrāqiyyīn and Baṣriyyīn",
+            "B": "Baṣriyyīn and Kūfiyyīn",
+            "C": "ʿIrāqiyyīn and Kūfiyyīn",
+            "D": "Khurasāniyyīn and ʿIrāqiyyīn",
+        },
+        "answer": "D",
+    },
+    {
+        "question": "The integrals of Wuḍūʾ in the Shāfiʿī Madhhab are:",
+        "options": {
+            "A": "7",
+            "B": "4",
+            "C": "6",
+            "D": "10",
+        },
+        "answer": "C",
+    },
+    {
+        "question": "Which great Shāfiʿī scholar held that the feet of the woman are not ʿAwrah?",
+        "options": {
+            "A": "al-Ghazālī",
+            "B": "al-Muzanī",
+            "C": "al-Rabīʿ",
+            "D": "al-Juwaynī",
+        },
+        "answer": "B",
+    },
+    {
+        "question": "The Imām of the ʿIrāqī path was:",
+        "options": {
+            "A": "Abū Ḥāmid al-Isfarāyīnī",
+            "B": "Abū Isḥāq al-Isfarāyīnī",
+            "C": "Abū Ḥāmid al-Ghazālī",
+            "D": "Abū Isḥāq al-Shīrāzī",
+        },
+        "answer": "A",
+    },
+    {
+        "question": "The Arkān of Ṣalāh are:",
+        "options": {
+            "A": "13",
+            "B": "15",
+            "C": "20",
+            "D": "11",
+        },
+        "answer": "A",
+    },
+    {
+        "question": "The baseline for Shāfiʿī Fatwā upon the Muʿtamad is:",
+        "options": {
+            "A": "al-Rawḍah",
+            "B": "al-Muḥarrar",
+            "C": "al-Minhāj",
+            "D": "al-Wajīz",
+        },
+        "answer": "C",
+    },
+    {
+        "question": "The author of the great Fiqh book “Baḥr al-Madhhab” (Sea of the Madhhab) was:",
+        "options": {
+            "A": "al-Rūyānī",
+            "B": "Ibn Khuzaymah",
+            "C": "al-Shirbīnī",
+            "D": "al-Mutawallī",
+        },
+        "answer": "A",
+    },
+]
+
+
+if __name__ == "__main__":
+    pass
